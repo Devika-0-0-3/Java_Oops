9095a54498026032e42b90b6a9b8edf85459a60b
Sameera-0-0-3 <mailtodevika003.com>
2025-01-17 22:57:06 +0530

    non-parametrized constructor

--- sample.java
+++ sample.java
@@ -15,6 +15,26 @@ class Pen{
 
 
 }
+//classname Capital letter
+//functionsname small letter
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    //constructor for Student class
+    //non-parametrized constructor
+    Student(){
+        System.out.println("Constructor called");
+    }
+
+}
+
+
+
 //main function comes under public class
 public class sample {
     public static void main(String args[]){
@@ -30,6 +50,21 @@ public class sample {
 
         pen2.printColor();
         pen1.printColor();
+
+        Student s1=new Student(); //Student constructor defined above so when object is created it prints whatever in Student constructor
+        //new keyword -->memory heap one space is allocated and there these objects get stored there
+        //Student() these are constructors, they construct object
+        //3 constructors-->non-parametrized, 
+        /*Properties of constructors:
+        Classname and constructor name ssame
+        They dont return anything and they dont have any return type ,they only construct object
+        Constructors can be called once when it is created
+        */
+
+        s1.name="Devika";
+        s1.age=40;
+        s1.printInfo();
+
 
     }
    
