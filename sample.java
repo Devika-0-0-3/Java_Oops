14619755957979bebb67963e328fee9b99eeb091
Sameera-0-0-3 <mailtodevika003.com>
2025-01-18 00:09:57 +0530

    Comment on destructors

--- sample.java
+++ sample.java
@@ -1,3 +1,5 @@
+//class,objects,this,constructors
+
 class Pen{ 
     //class have data-->string int data stored in it and members->functions 
     //defining blueprint of pen
@@ -26,19 +28,27 @@ class Student{
         System.out.println(this.age);
     }
     //constructor for Student class
-    //non-parametrized constructor
+   
     /* non parametrized constructor
     Student(){
         System.out.println("Constructor called");
     }
         */
     //even if it is not written java bydefault makes a constructor and calls it
-
+    
+    /*parametrized constructor 
     Student(String name,int age){
       //object name 
         this.name=name; //parameter name
         this.age=age;
     }
+        */
+    //copy constructor
+    Student(Student s2){
+        this.name=s2.name;
+        this.age=s2.age;
+    }
+    Student(){}
 }
 
 
@@ -59,25 +69,43 @@ public class sample {
         pen2.printColor();
         pen1.printColor();
 
-        //Student s1=new Student(); 
+        
        //Student constructor defined above so when object is created it prints whatever in Student constructor
         //new keyword -->memory heap one space is allocated and there these objects get stored there
         //Student() these are constructors, they construct object
-        //3 constructors-->non-parametrized, parametrized constructor, 
+        //3 constructors-->non-parametrized, parametrized constructor, copy constructors(one object is copied and put into another object)
         /*Properties of constructors:
         Classname and constructor name ssame
         They dont return anything and they dont have any return type ,they only construct object
         Constructors can be called once when it is created
         */
 
+        //non-parametrized
+        //Student s1=new Student(); 
        // s1.name="Devika";
        // s1.age=40;
+       //s1.printInfo()
+
        //parametrized:
-        Student s2=new Student("Anu",45);
+        //Student s2=new Student("Anu",45);
+        //s2.printInfo();
+
+        //copy constructor object creation
+        Student s1=new Student(); // as there is a copy constructor we should also make a constructor for this also
+        s1.name="fafa";
+        s1.age=24;
+
+        Student s2=new Student(s1); //copy constructor
         s2.printInfo();
+
+
 
 
     }
    
     
 }
+//destructors does need to write  but in c++ it is need to write
+//in java it is automatic 
+//automatic garbage collection-->if an object is not used in the cod ein further it is deleted
+//so we dont write destructors in java it uses garbage collection
