28465f5902b4d7ce4c8351c0b94c51adad8865aa
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-05-15 15:14:41 +0530

    Oops concept

---
+++ Ho.java
@@ -0,0 +1,15 @@
+//pure abstraction means hides all useless information , only useful information will be shown to users and is done using interface
+//in interface, constructor cannot be created
+//interface is implemented in java not extended
+//interface cannot have non abstract methods
+interface   Animal{
+    void walk();
+    void eat();
+}
+public class Ho
+
+public class Encap_interface {
+    public static void main(String args[]){
+
+    }
+}
